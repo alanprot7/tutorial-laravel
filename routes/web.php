26630923a085d5491c7f01410e2e04ed9157a6ec
Alan Oliveira <alanprot7@gmail.com>
2022-11-26 14:13:54 -0300

    aula 04

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $nome = "Alan";
+    $idade = 42;
+
+    return view('welcome', ['nome' => $nome, 'idade' => $idade, 'profissao' => "programador" ]);
 });
 
 
