b6586ca84c9c0a750d7cb99b1074d6dee029f100
Alan Oliveira <alanprot7@gmail.com>
2022-12-09 23:44:43 -0300

    aula 21

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,13 +7,18 @@
 
 <div id="search-container" class="col-md-12">
     <h1>Busque um evento</h1>
-    <form action="">
+    <form action="/" method="GET">
         <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
     </form>
 </div>
 <div id="events-container" class="col-md-12">
-    <h2>Proximos Eventos</h2>
-    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    @if ($search)
+        <h2>Buscando por: {{ $search }}</h2>
+    @else
+        <h2>Proximos Eventos</h2>
+        <p class="subtitle">Veja os eventos dos próximos dias</p>
+    @endif
+    
     <div id="cards-container" class="row">
         @foreach ($events as $event)
             <div class="card col-md-3">
@@ -26,7 +31,9 @@
                 </div>
             </div>        
         @endforeach
-        @if (count($events) == 0)
+        @if (count($events) == 0 && $search)
+            <p>Não possível encontrar nenhum evento com {{ $search }}!</p>&nbsp;<a href="/">Ver todos</a>
+        @elseif (count($events) == 0)
             <p>Não há eventos disponíveis</p>
         @endif
     </div>
