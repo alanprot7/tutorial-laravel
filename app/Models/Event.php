46fadd106f90957d088091dc17ff041078f25d3e
Alan Oliveira <alanprot7@gmail.com>
2022-12-12 21:11:03 -0300

    aula 28

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Event extends Model
 {
@@ -19,5 +20,9 @@ class Event extends Model
 
     public function user() {
         return $this->belongsTo('App\Models\User');
+    }
+
+    public function users(){
+        return $this->belongsToMany('App\Models\User');
     }
 }
